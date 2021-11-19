0fbee8167f62860e3f2366e6a76bc1eaaafe1ebb
Gustavo <ovatsug.gu@hotmail.com>
2021-11-19 18:42:14 -0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <Systembar/>
+    <AppBar/>
+    
+    <v-main>
+     <Navdraw/>
+    </v-main>
+
+    <Footer/>
+    <Navbot/>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Footer from './components/Footer';
+import Navbot from './components/Navbot';
+import Systembar from './components/Systembar';
+import AppBar from './components/Appbar';
+import Navdraw from './components/Navdraw';
+
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Footer,
+    Navbot,
+    Systembar,
+    AppBar,
+    Navdraw,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
